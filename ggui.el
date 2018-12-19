1891c40d7c617e05c9420fd2e56fb2a256966ff2
Yuan Fu <casouri@gmail.com>
2018-12-19 13:36:44 -0500

    Change range to view

--- ggui.el
+++ ggui.el
@@ -109,6 +109,7 @@ LEVEL is a symbol, can be :info :warn or :error."
 (define-error 'ggui-pos-out-of-range "Position is out of the range of the buffer (pos showed after colon)" '(error))
 (define-error 'ggui-buffer-missing "There is no such buffer" '(file-missing error))
 (define-error 'ggui-end-of-line "The line is not long enough" '(end-of-buffer error))
+(define-error 'ggui-delimiter-misplace "A ggui delimiter should/should not be here." '(error))
 
 ;;;; Etc
 
@@ -116,7 +117,7 @@ LEVEL is a symbol, can be :info :warn or :error."
   "Go to POS in BUFFER or FILE.
 One of BUFFER or FILE has to be provided. If both presented, BUFFER is used.
 
-* Args
+* Arcs
 - LINE :: 0-based, nil means 0
 - COLUMN :: 0-based, nil means 0
 - BUFFER :: string or a buffer
@@ -145,89 +146,108 @@ nil
              nil)
     (end-of-buffer (signal 'ggui-pos-out-of-range (list "LINE:" line "COLUMN:" column)))))
 
-;;;; ggui-range
+;;;; ggui-view
 
 ;;;;; Class & init & helper
 
-(ggui-defclass ggui-range ()
+(ggui-defclass ggui-view ()
   ((overlay
     :documentation "Private. The overlay covering the range.
-Don't use `slot-value' on this, instead, use the accessor `ggui--overlay'."))
+Don't use `slot-value' on this, instead, use the accessor `ggui--overlay'.")
+   (text
+    :documentation "Public. The text of the view."))
   ;; It is covered by an overlay (slot), and must have line feed and after it (which is included).
-  "A range in a buffer.")
+  "A range in a buffer.
+Public slot: text
+Virtual slot:
+- beg           R&W
+- end           R&W
+- buffer        R
+- property-list R&W")
 
-(defun ggui-range-new (beg end &optional buffer property-list)
-  "Return a `ggui-range' object from BEG to END in BUFFER.
+(defun ggui-view-new (text &rest property-list)
+  "Return a `ggui-view' object with TEXT.
 
-Use current buffer if omitted.
+PROPERTY-LIST:
+Remaining arguments form a sequence of PROPERTY VALUE pairs
+for overlay properties to add to the result."
+  (ggui-view :text text
+             :overlay (apply #'ggui--make-overlay
+                             1 1
+                             (get-buffer-create " *ggui-tmp*")
+                             property-list)))
 
-PROPERTY-LIST is a list of overlay property & value pairs.
-It looks like ((PROPERTY VALUE) (PROPERTY VALUE)...)."
-  (ggui-range :overlay (ggui--make-overlay beg end buffer property-list)))
+(cl-defmethod ggui--presentp ((view ggui-view))
+  "Return t if VIEW is inserted into some buffer, nil if not."
+  ;; if the overlay is in tmp buffer, it is no present.
+  (not (eq (overlay-buffer (ggui--overlay view)) (get-buffer-create " *ggui-tmp*"))))
 
-(defun ggui-translate-pos (line column buffer)
+(defun ggui--translate-pos (line column buffer)
   "Translate LINE:COLUMN in BUFFER to POS in BUFFER.
 Return (POS BUFFER)."
   (cons (progn (ggui-goto line column buffer) (point))
         buffer))
 
-(defun ggui--make-overlay (beg end &optional buffer property-list)
+(defun ggui--make-overlay (beg end &optional buffer &rest property-list)
   "Return an overlay from BEG to END in BUFFER.
 
 See `ggui-decode-pos' for options on BEG and END.
-
-Use current buffer if BUFFER is nil.
-For PROPERTY-LIST see `ggui-range-new'."
+Use current buffer if BUFFER is nil. PROPERTY-LIST:
+Remaining arguments form a sequence of PROPERTY VALUE pairs
+for overlay properties to add to the result."
   (let ((overlay (make-overlay beg end buffer nil t)))
     ;; marker: insert in front: included, end: included
-    (mapc (lambda (lst) (overlay-put overlay (car lst) (cadr lst)))
-          property-list)
+    (while property-list
+      (overlay-put overlay (pop property-list) (pop property-list)))
     overlay))
 
 ;;;;; Beg & end & buffer
 
-(cl-defmethod ggui--beg-mark ((range ggui-range))
-  "Return the beginning of RANGE as a marker."
-  (let ((overlay (ggui--overlay range)))
-    (set-marker (make-marker) (overlay-start overlay) (overlay-buffer overlay))))
+;; enable if they are actually needed
 
-(cl-defmethod ggui--end-mark ((range ggui-range))
-  "Return the end of RANGE as a marker."
-  (let ((overlay (ggui--overlay range)))
-    (set-marker (make-marker) (overlay-end overlay) (overlay-buffer overlay))))
+;; (cl-defmethod ggui--beg-mark ((view ggui-view))
+;;   "Return the beginning of VIEW as a marker."
+;;   (let ((overlay (ggui--overlay view)))
+;;     (set-marker (make-marker) (overlay-start overlay) (overlay-buffer overlay))))
 
-(cl-defmethod ggui--beg ((range ggui-range))
-  "Return the beginning of RANGE as a point."
-  (overlay-start (ggui--overlay range)))
+;; (cl-defmethod ggui--end-mark ((view ggui-view))
+;;   "Return the end of VIEW as a marker."
+;;   (let ((overlay (ggui--overlay view)))
+;;     (set-marker (make-marker) (overlay-end overlay) (overlay-buffer overlay))))
 
-(cl-defmethod (setf ggui--beg) (beg (range ggui-range))
-  "Set BEG of RANGE."
-  (ggui-update-pos range beg))
+;; (cl-defmethod ggui--beg ((view ggui-view))
+;;   "Return the beginning of VIEW as a point."
+;;   (overlay-start (ggui--overlay view)))
 
-(cl-defmethod ggui--end ((range ggui-range))
-  "Return the end of RANGE as a point."
-  (overlay-end (ggui--overlay range)))
+;; (cl-defmethod (setf ggui--beg) (beg (view ggui-view))
+;;   "Set BEG of VIEW."
+;;   (ggui-move-overlay view beg))
 
-(cl-defmethod (setf ggui--end) (end (range ggui-range))
-  "Set END of RANGE."
-  (ggui-update-pos range end))
+;; (cl-defmethod ggui--end ((view ggui-view))
+;;   "Return the end of VIEW as a point."
+;;   (overlay-end (ggui--overlay view)))
 
-(cl-defmethod ggui--buffer ((range ggui-range))
-  "Return the buffer of RANGE."
-  (overlay-buffer (ggui--overlay range)))
+;; (cl-defmethod (setf ggui--end) (end (view ggui-view))
+;;   "Set END of VIEW."
+;;   (ggui-move-overlay view end))
 
-(cl-defmethod ggui-update-pos ((range ggui-range) beg end &optional buffer)
-  "Update the range of RANGE to BEG to END in BUFFER.
+;; (cl-defmethod ggui--buffer ((view ggui-view))
+;;   "Return the buffer of VIEW."
+;;   (overlay-buffer (ggui--overlay view)))
+
+(cl-defmethod ggui--move-overlay ((view ggui-view) beg end &optional buffer)
+  "Move overlay of VIEW to BEG END in BUFFER.
 If BEG, END or BUFFER omitted, don't change it.
-
 Return nil."
   ;; basically create a new one, set values, delete the old one
-  (let ((old-ov (ggui--overlay range))
+  (let ((old-ov (ggui--overlay view))
         new-ov)
-    (setq new-ov (ggui--make-overlay (or beg (overlay-start old-ov))
-                                     (or end (overlay-end old-ov))
-                                     (or buffer (overlay-buffer old-ov))))
-    (setf (ggui--overlay range) new-ov)
+    (setq new-ov (apply #'ggui--make-overlay
+                        (or beg (overlay-start old-ov))
+                        (or end (overlay-end old-ov))
+                        (or buffer (overlay-buffer old-ov))
+                        (overlay-properties old-ov)))
+    (setf (ggui--overlay view) new-ov)
     (delete-overlay old-ov)) ; Don't forget to cleanup -- Izayoi Sakuya
   nil)
 
@@ -253,38 +273,80 @@ When return, point is at the end of delimiters."
 
 ;;;;; Put/insert before/after
 
-(cl-defmethod ggui-put-before ((range ggui-range) (str string))
-  "Insert STR in front of RANGE. RANGE doesn't cover STR.
+(defun ggui--check-delimiter (&optional no-left no-right)
+  "Check if delimiters are correctly setup.
+Should: one and only one before point, one and only one after point."
+  (let ((point (point)))
+    (cond ((not (member 'ggui-delimiter (text-properties-at (1- point))))
+           (signal 'ggui-delimiter-misplace (list "Left delimiter is missing, point at" point "delimiter should be at" (1- point))))
+          ((member 'ggui-delimiter (text-properties-at (- point 2)))
+           (signal 'ggui-delimiter-misplace (list "Extra delimiter left to left delimiter, point at" point "extra delimiter point at" (- point 2))))
+          ((not (member 'ggui-delimiter (text-properties-at point)))
+           (signal 'ggui-delimiter-misplace (list "Right delimiter is missing, point at" point "delimiter should be at" point)))
+          ((member 'ggui-delimiter (text-properties-at (1+ point)))
+           (signal 'ggui-delimiter-misplace (list "Extra delimiter right to right delimiter, point at" point "extra delimiter point at" (1+ point)))))))
+
+;;;;;; String
+
+(cl-defmethod ggui-put-before ((view ggui-view) (str string))
+  "Insert STR in front of VIEW. VIEW doesn't cover STR.
 Return nil."
   (ggui-edit
-   (goto-char (1- (ggui--beg range)))
-   ;; now we are between the two delimiters hiahiahia
+   (goto-char (1- (ggui--beg view)))
+   ;; now we are between the two delimiters
+   (ggui--check-delimiter)
    (ggui--insert-2delimiter)
    (backward-char)
    (insert str)))
 
-(cl-defmethod ggui-put-after ((range ggui-range) (str string))
-  "Insert STR in front of RANGE. RANGE doesn't cover STR.
+(cl-defmethod ggui-put-after ((view ggui-view) (str string))
+  "Insert STR in front of VIEW. VIEW doesn't cover STR.
 Return nil."
   (ggui-edit
-   (goto-char (1+ (ggui--end range)))
+   (goto-char (1+ (ggui--end view)))
+   (ggui--check-delimiter)
    (ggui--insert-2delimiter)
    (forward-char)
    (insert str)))
 
-(cl-defmethod ggui-insert-before ((range ggui-range) (str string))
-  "Insert STR in front of RANGE. RANGE cover STR.
+(cl-defmethod ggui-insert-before ((view ggui-view) (str string))
+  "Insert STR in front of VIEW. VIEW cover STR.
 Return nil."
   (ggui-edit
-   (goto-char (ggui--beg range))
+   (goto-char (ggui--beg view))
    (insert str)))
 
-(cl-defmethod ggui-insert-after ((range ggui-range) (str string))
-  "Insert STR in front of RANGE. RANGE doesn't cover STR.
+(cl-defmethod ggui-insert-after ((view ggui-view) (str string))
+  "Insert STR in front of VIEW. VIEW doesn't cover STR.
 Return nil."
   (ggui-edit
-   (goto-char (ggui--end range))
+   (goto-char (ggui--end view))
    (insert str)))
+
+;;;;;; View
+
+(cl-defmethod ggui-put-before ((view ggui-view) (aview ggui-view))
+  "Insert STR in front of VIEW. VIEW doesn't cover STR.
+Return nil."
+  (ggui-edit
+   (goto-char (1- (ggui--beg view)))
+   ;; now we are between the two delimiters
+   (ggui--check-delimiter)
+   (ggui--insert-2delimiter)
+   (backward-char)
+   (ggui--move-overlay aview (point) (point))
+   (insert (ggui-text aview))))
+
+(cl-defmethod ggui-put-after ((view ggui-view) (aview ggui-view))
+  "Insert STR in front of VIEW. VIEW doesn't cover STR.
+Return nil."
+  (ggui-edit
+   (goto-char (1+ (ggui--end view)))
+   (ggui--check-delimiter)
+   (ggui--insert-2delimiter)
+   (forward-char)
+   (ggui--move-overlay aview (point) (point))
+   (insert (ggui-text aview))))
 
 (provide 'ggui)
 
