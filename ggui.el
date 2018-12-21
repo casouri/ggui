0211a266e80310ecf20d14f5e559ad26c34a9ef2
Yuan Fu <casouri@gmail.com>
2018-12-21 15:46:26 -0500

    Update qualifiers to sequence

--- ggui.el
+++ ggui.el
@@ -534,6 +534,16 @@ If N is negative, toggle backward N times."
     (signal 'ggui-view-not-present)))
 
 ;;;; List and tree
+;;
+;; Implement these generic functions for sequence:
+;;
+;; ggui-seq-delete
+;; ggui-seq-append
+;; ggui-insert-at
+;; seq-elt
+;; seq-find
+;; seq-length
+;; (setf seq-elt)
 
 (cl-defgeneric ggui-seq-delete (elt seq)
   "Delete ELT from SEQ.")
@@ -573,7 +583,7 @@ Return the modified seq."
 ;; We don't need to remove view before adding it because
 ;; it is done automatically for us in `ggui-put-after/before'
 
-(cl-defmethod ggui-put-at (view (seq list) n)
+(cl-defmethod ggui-put-at (view (seq sequence) n)
   "Put VIEW at N in SEQ (a list of `ggui-view's).
 VIEW is not added again if it's already in seq,
 instead, it is moved to N.
@@ -603,7 +613,7 @@ Return SEQ."
   seq)
 
 ;; seq to view
-(cl-defmethod ggui-put-before ((seq list) (view ggui-view))
+(cl-defmethod ggui-put-before ((seq sequence) (view ggui-view))
   "Put every view in SEQ before VIEW, in normal order.
 E.g., SEQ: (1 2 3 4) VIEW: 5 -> 1 2 3 4 5.
 This function is recursive.
@@ -620,7 +630,7 @@ Return nil."
       (setq last-right this)))
   nil)
 
-(cl-defmethod ggui-put-after ((seq list) (view ggui-view))
+(cl-defmethod ggui-put-after ((seq sequence) (view ggui-view))
   "Put every view in SEQ after VIEW, in normal order.
 E.g., SEQ: (2 3 4 5) VIEW: 1 -> 1 2 3 4 5.
 This function is recursive.
@@ -637,24 +647,24 @@ This function is recursive.
   nil)
 
 ;; view to seq
-(cl-defmethod ggui-put-before ((view ggui-view) (seq list))
+(cl-defmethod ggui-put-before ((view ggui-view) (seq sequence))
   "Put VIEW before the first element of SEQ.
 Return nil."
   (ggui-put-before view (seq-elt seq 0))
   nil)
 
-(cl-defmethod ggui-put-after ((view ggui-view) (seq list))
+(cl-defmethod ggui-put-after ((view ggui-view) (seq sequence))
   "Put VIEW after the last element of SEQ.
 Return nil."
   (ggui-put-after view (seq-elt seq (1- (seq-length seq))))
   nil)
 
-(cl-defmethod ggui-append ((seq1 list) (seq2 list))
+(cl-defmethod ggui-append ((seq1 sequence) (seq2 sequence))
   "Append two list of `ggui-view's and return the merged list."
   (ggui-put-after (seq-elt seq1 (1- (seq-length seq1))) seq2)
   (ggui-seq-append seq1 seq2))
 
-(cl-defmethod ggui-delete (view (seq list))
+(cl-defmethod ggui-delete (view (seq sequence))
   "Remove VIEW from SEQ.
 VIEW can be either a `ggui-view' or a list of them"
   (unless (or (cl-typep view 'ggui-view)
@@ -681,7 +691,7 @@ Depth first, left first."
       (apply #'ggui-apply-tree func cdr args)
       (apply func cdr args))))
 
-(cl-defmethod ggui--remove-display ((seq list))
+(cl-defmethod ggui--remove-display ((seq sequence))
   "Remove display of SEQ.
 This function is recursive."
   (seq-map #'ggui--remove-display seq))
