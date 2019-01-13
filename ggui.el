04635e1ffe1e917239c8880b64158a06f9425749
Yuan Fu <casouri@gmail.com>
2019-01-13 17:42:11 -0500

    Move goto function to ggud

--- ggui.el
+++ ggui.el
@@ -252,40 +252,6 @@ Each function in the hook is called with last-buffer and this-buffer.")
 (define-error 'ggui-view-not-present "This view is not on any buffer." '(error))
 (define-error 'ggui-element-missing "The element is assumed to be in a list, but it is not" '(error))
 
-;;;; Etc
-
-(defun ggui-goto (line column &optional buffer file)
-  "Go to POS in BUFFER or FILE.
-One of BUFFER or FILE has to be provided. If both presented, BUFFER is used.
-
-* Arcs
-- LINE :: 0-based, nil means 0
-- COLUMN :: 0-based, nil means 0
-- BUFFER :: string or a buffer
-- FILE :: absolute path to file
-
-* Return
-nil
-
-* Error
-- `pos-out-of-range' :: if POS is out of the range of the buffer it's in.
-- `file-missing'
-- `buffer-missing'"
-  ;; switch to buffer/file
-  (cond (buffer  (switch-to-buffer (or (get-buffer buffer) (signal 'ggui-buffer-missing buffer))))
-        (file (if (file-exists-p file) (find-file file) (signal 'file-missing file))) ;; TODO find-file-literally?
-        (t (signal 'wrong-number-of-arguments (list "You need to supply either BUFFER or FILE, however, both are nil."))))
-  ;; go to position
-  (goto-char 1)
-  (condition-case nil
-      (progn (forward-line (or line 0))
-             ;; dotimes is inclusive
-             (dotimes (_ (or column 0))
-               (if (eq (char-after) ?\n)
-                   (signal 'ggui-end-of-line (list "LINE" line "in BUFFER" buffer "doesn't have COLUMN" column))
-                 (forward-char)))
-             nil)
-    (end-of-buffer (signal 'ggui-pos-out-of-range (list "LINE:" line "COLUMN:" column)))))
 
 ;;;; ggui-view
 
